ca20d44bd12052011e9e5085009550177c2817b0
CWMMO <alanpenguino@gmail.com>
2025-01-18 13:20:43 -0500

    Update chatPacketListener.java

--- src/main/java/com/cwmmo/skCWMMO/events/listeners/chatPacketListener.java
+++ src/main/java/com/cwmmo/skCWMMO/events/listeners/chatPacketListener.java
@@ -1,4 +1,23 @@
 package com.cwmmo.skCWMMO.events.listeners;
 
-public class chatPacketListener {
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import com.cwmmo.skCWMMO.events.PlayerChatReceiveEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+public class chatPacketListener extends PacketAdapter {
+    public chatPacketListener(Plugin plugin) {
+        super(plugin, PacketType.Play.Server.CHAT);
+    }
+
+    @Override
+    public void onPacketSending(PacketEvent event) {
+        Player player = event.getPlayer();
+        String message = event.getPacket().getStrings().read(0);
+
+        new PlayerChatReceiveEvent(player, message).PlayerChatReceive(player, message);
+        // listen for chat receive packets and fire the event
+    }
 }
